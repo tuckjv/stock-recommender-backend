75f7ed6a7b845a4a88252953b8897e65a9a6dd40
Tucker <tuckjvr@gmail.com>
2022-08-13 16:01:03 -0400

    index.js

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(sslRedirect());
 app.use(express.json());
 
 app.post('/', async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.tuckersstocks.site");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   let tickers = [req.body.first, req.body.second, req.body.third, req.body.fourth, req.body.fifth];
   let reccomend = [];
   let notRec = [];
