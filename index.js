51b4108d2557da8564bab181b114848934e60a29
Tucker <tuckjvr@gmail.com>
2022-08-13 15:14:41 -0400

    changed origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
 }));
 
 app.post('/', async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.tuckersstocks.site");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
